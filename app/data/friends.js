d771d6a756e9295bca7bbb77bf5a5a34cc33c4d5
Daryl Acut <darylacut@yahoo.com>
2019-08-19 17:37:52 -0400

    updated firends

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,8 +2,8 @@
 
 var friendsArray = [
     {
-        name: "Ahmed",
-        photo: "https://images.unsplash.com/photo-1518806118471-f28b20a1d79d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
+        name: "Jonathan",
+        photo: "https://pbs.twimg.com/profile_images/1056435484979748864/nTZxzBEP_400x400.jpg",
         scores :[
             5,
             1,
@@ -18,10 +18,17 @@ var friendsArray = [
       ]
     },
     {
-      name: "Abdi",
-      photo: "https://i.pinimg.com/originals/7d/1a/3f/7d1a3f77eee9f34782c6f88e97a6c888.jpg",
+      name: "Trevor",
+      photo: "http://www.game-art-hq.com/wp-content/uploads/Alucard-Castlevania-Lords-of-Shadow-2-Portrait-Art.jpg",
       scores : [
         4,2,5,1,2,3,4,2,3,1
+      ]
+    }
+    {
+      name: "Gabriel",
+      photo: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/922441a8-1546-433c-bf0b-6f02f9ee6d87/d91khs5-f0127ceb-d500-45c4-9296-06af5730c032.jpg/v1/fill/w_1023,h_614,q_75,strp/gabriel_dracula_by_lucasrex_d91khs5-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NjE0IiwicGF0aCI6IlwvZlwvOTIyNDQxYTgtMTU0Ni00MzNjLWJmMGItNmYwMmY5ZWU2ZDg3XC9kOTFraHM1LWYwMTI3Y2ViLWQ1MDAtNDVjNC05Mjk2LTA2YWY1NzMwYzAzMi5qcGciLCJ3aWR0aCI6Ijw9MTAyMyJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.5q4T7VvHxPt_-bqSsVnWLOaa2UbO4amLDq2HqTcOFBg",
+      scores : [
+        3,3,3,3,3,3,3,3,3,3
       ]
     }
   ];
